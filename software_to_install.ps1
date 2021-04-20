250329f48b3ba3e8461ad2f7316dad29257b1920
Liam <37161087+Liam-Entasker@users.noreply.github.com>
2021-04-20 21:25:25 +1000

    Add files via upload

---
+++ software_to_install.ps1
@@ -0,0 +1,16 @@
+#####
+# install Chocolatey and then software for new vm
+##### 
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Software
+choco install chrome-remote-desktop-chrome -y
+choco install git -y
+choco install visualstudiocode -y
+choco install visualstudio2019professional -y
+choco install teamviewer -y
+
+# choco install sql-server-2019
+# choco install sql-server-2019-cumulative-update
