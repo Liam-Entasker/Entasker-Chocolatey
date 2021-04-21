feae6f7c741df3087155a0b272152cd6b568181a
Liam <37161087+Liam-Entasker@users.noreply.github.com>
2021-04-21 11:24:03 +1000

    Update software_to_install.ps1

--- software_to_install.ps1
+++ software_to_install.ps1
@@ -6,11 +6,14 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Software
-choco install googlechrome -y
+choco install googlechrome -y --ignore-checksums
 choco install git -y
 choco install visualstudiocode -y
 choco install visualstudio2019professional -y
 choco install teamviewer -y
+
+choco install powershell-core -y
+choco install dbatools -y
 
 # choco install sql-server-2019
 # choco install sql-server-2019-cumulative-update
